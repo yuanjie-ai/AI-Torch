ad2ea6d17d20f88df577cdbae686ab66212a7875
尐 <313303303@qq.com>
2018-12-17 18:14:23 +0800

    Create fit.py

---
+++ utils/fit.py
@@ -0,0 +1,24 @@
+def fit(model, epochs=5, batch_size=128, loss_func=torch.nn.CrossEntropyLoss, optimizer=torch.optim.Adam):
+    from tqdm import tqdm_notebook
+    from sklearn.metrics import roc_auc_score as auc
+    optimizer = optimizer(model.parameters())
+    bar_epochs = tqdm_notebook(range(epochs))
+    for e in bar_epochs:
+        bar_epochs.set_description(f"Epoch {e}:")
+        t = tqdm_notebook(range(0, t_X.size(0), batch_size))
+        for b in t:  # for each training step
+            # train your data...
+            b_X = t_X[b:b + batch_size]
+            b_y = t_y[b:b + batch_size]
+            output = net(b_X)  # rnn output
+            loss = loss_func(
+                output,
+                b_y.long().view(-1))  # cross entropy loss and y is not one-hotted
+            optimizer.zero_grad()  # clear gradients for this training step
+            loss.backward()  # backpropagation, compute gradients
+            optimizer.step()
+            if b % 10000 == 0:
+                t.set_description(
+                    f"Epoch {e}:"
+                    f"Loss: {loss.data.numpy():.5f} | "
+                    f"Auc: {auc(b_y.numpy(), output.data.numpy()[:, 1]):.5}")
